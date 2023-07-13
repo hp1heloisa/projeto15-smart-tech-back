91e8a5cc1f1b30ae7e7d35937fd3e482fb2dd817
hp1heloisa <heloisapimentel2202@gmail.com>
2023-07-13 19:20:13 -0300

    bug .env ok

--- src/schemas/userSchemas.js
+++ src/schemas/userSchemas.js
@@ -2,12 +2,12 @@ import Joi from "joi";
 
 export const schemaCadastro = Joi.object({
     name: Joi.string().required(),
-    email: Joi.email().required(),
-    password: Joi.min(3).string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(3).required(),
     adress: Joi.string().required()
 });
 
 export const schemaLogin = Joi.object({
-    email: Joi.email().required(),
-    password: Joi.min(3).string().required()
+    email: Joi.string().email().required(),
+    password: Joi.string().min(3).required()
 });
