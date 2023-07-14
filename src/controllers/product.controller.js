2842a1055acc8482246417d11449672a3e137760
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2023-07-13 23:45:39 -0300

    Buscar produtos

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import { db } from "../database/database.connection.js";
+import { ObjectId } from "mongodb"
 
 export async function postProduct(req, res) {
     const {name, image, value, category, description} = req.body;
@@ -9,6 +10,26 @@ export async function postProduct(req, res) {
 
         await db.collection('products').insertOne({name, image, value, category, description});
         res.sendStatus(201);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+export async function getProduct(req, res) {
+    try {
+        const products = await db.collection("products").find().toArray()
+        res.send(products)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function getProductById(req, res) {
+    const { id } = req.body;
+    
+    try {
+        const product = await db.collection("products").findOne({ _id: new ObjectId(id) })
+        res.send(product);
     } catch (error) {
         res.status(500).send(error.message);
     }
